f7c372f8995799ec3eacc4769b041bb35ed2a2b0
rehanroot <rehanroot2@gmail.com>
2024-10-02 01:09:21 -0400

    added background image to login page (Modify the login page)

--- listify-frontend/src/components/UserLogin.js
+++ listify-frontend/src/components/UserLogin.js
@@ -30,12 +30,10 @@ const UserLogin = () => {
 
     return (
         <div className="login-container">
-            <img src="your-logo-url.png" alt="Logo" className="logo" /> {/* Add your logo */}
             <h1 className="login-title">Welcome Back</h1>
             {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
             <form onSubmit={handleSubmit} className="login-form">
-                <div className="input-group">
-                    <label htmlFor="username">Email or Username</label>
+                <div className="input-group floating-label">
                     <input
                         type="text"
                         id="username"
@@ -43,38 +41,38 @@ const UserLogin = () => {
                         onChange={(e) => setUsername(e.target.value)}
                         required
                         className="input-field"
+                        placeholder=" " // Trigger floating label
                     />
+                    <label htmlFor="username" className="floating-label-text">Email or Username</label>
                 </div>
-                <div className="input-group">
-                    <label htmlFor="password">Password</label>
+                <div className="input-group floating-label">
                     <input
                         type={showPassword ? 'text' : 'password'}
                         id="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
-                        className="input-field" // Ensuring same class as username input
+                        className="input-field"
+                        placeholder=" " // Trigger floating label
                     />
+                    <label htmlFor="password" className="floating-label-text">Password</label>
                     <button type="button" onClick={togglePasswordVisibility} className="toggle-password">
                         {showPassword ? 'Hide' : 'Show'}
                     </button>
-                </div>
-                <div className="remember-me">
-                    <input type="checkbox" id="remember" />
-                    <label htmlFor="remember">Remember Me</label>
                 </div>
                 <button type="submit" disabled={isLoading} className="login-button">
                     {isLoading ? 'Loading...' : 'Log In'}
                 </button>
             </form>
+            <div className="social-login">
+                <button className="social-button facebook-button">Log in with Facebook</button>
+                <button className="social-button google-button">Log in with Google</button>
+                <button className="social-button twitter-button">Log in with Twitter</button>
+            </div>
             <div className="links">
                 <a href="/forgot-password" className="link">Forgot Password?</a>
                 <span className="separator"> | </span>
                 <a href="/register" className="link">Create an Account</a>
-            </div>
-            <div className="social-login">
-                <button className="social-button">Log in with Google</button>
-                <button className="social-button">Log in with Facebook</button>
             </div>
         </div>
     );
